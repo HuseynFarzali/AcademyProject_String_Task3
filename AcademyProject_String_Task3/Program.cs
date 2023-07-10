0f0e7492658481db932a2c1ff2ab3ba47e51c766
HuseynFarzali <huseynyf@code.edu.az>
2023-07-10 03:16:48 -0700

    Add project files.

---
+++ AcademyProject_String_Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace AcademyProject_String_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the phrase: ");
+            string phrase = Console.ReadLine();
+
+            int len = phrase.Length;
+            StringBuilder newPhrase = new StringBuilder(phrase);
+
+            for (int i = 0; i < len; i++)
+            {
+                newPhrase[i] = char.Parse(phrase[len - i - 1].ToString().ToUpper());
+            }
+
+            string finalString = newPhrase.ToString();
+
+            Console.WriteLine(finalString);
+        }
+    }
+}
